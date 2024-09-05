98b42469f4cb2348fbec6c045ed876feb3c790c7
Allyson-English <allyson.english@gmail.com>
2024-09-05 15:58:43 -0400

    remove fs

--- src/dimo.ts
+++ src/dimo.ts
@@ -19,13 +19,8 @@ import {
 import { DimoWeb3Client } from "./web3/index";
 
 import { Stream } from "./streamr";
-import { ClientConfigDimo, ContractType, DIMO_APIs, ENVIRONMENT, SupportedNetworks } from "./utils/types";
-import {
-  CHAIN_ABI_MAPPING,
-  CHAIN_TO_NETWORK_ENUM_MAPPING,
-  ENV_TO_API_MAPPING,
-  api_mapping_by_env,
-} from "./utils/constants";
+import { ClientConfigDimo, ContractType, DIMO_APIs, ENVIRONMENT } from "./utils/types";
+import { CHAIN_ABI_MAPPING, ENV_TO_API_MAPPING, api_mapping_by_env } from "./utils/constants";
 
 export class DIMO {
   public attestation: Attestation;
@@ -44,7 +39,10 @@ export class DIMO {
   public web3: DimoWeb3Client;
 
   constructor(config: ClientConfigDimo) {
-    const apiMapping = ENV_TO_API_MAPPING[api_mapping_by_env.get(config.environment) || ENVIRONMENT.DEV];
+    const env = api_mapping_by_env.get(config.environment) || ENVIRONMENT.DEV;
+    const apiMapping = ENV_TO_API_MAPPING[env];
+    const abiMapping = CHAIN_ABI_MAPPING[env];
+
     this.identity = new Identity(apiMapping[DIMO_APIs.IDENTITY].url);
     this.telemetry = new Telemetry(apiMapping[DIMO_APIs.TELEMETRY].url);
 
@@ -58,10 +56,6 @@ export class DIMO {
     this.devices = new Devices(apiMapping[DIMO_APIs.DEVICES].url);
     this.events = new Events(apiMapping[DIMO_APIs.EVENTS].url);
 
-    const abiMapping =
-      CHAIN_ABI_MAPPING[
-        CHAIN_TO_NETWORK_ENUM_MAPPING.get(config.chain.name ? config.chain.name : "") as SupportedNetworks
-      ];
     this.tokenexchange = new TokenExchange(
       apiMapping[DIMO_APIs.TOKEN_EXCHANGE].url,
       abiMapping.contracts[ContractType.DIMO_REGISTRY].address
@@ -75,43 +69,43 @@ export class DIMO {
     /**
      * Set up Web3 Client
      */
-    this.web3 = new DimoWeb3Client(config);
+    this.web3 = new DimoWeb3Client(env);
   }
 
   // Helper Function
-  async authenticate() {
-    let fs;
-    try {
-      // Dynamically import fs
-      if (typeof process !== "undefined" && process.versions && process.versions.node) {
-        fs = await import("fs");
-      } else {
-        // Optionally handle the case where 'fs' is not available, returns null
-        console.log("Not in Node.js environment; `fs` module is not available.");
-        return null;
-      }
+  // async authenticate() {
+  //   let fs;
+  //   try {
+  //     // Dynamically import fs
+  //     if (typeof process !== "undefined" && process.versions && process.versions.node) {
+  //       fs = await import("fs");
+  //     } else {
+  //       // Optionally handle the case where 'fs' is not available, returns null
+  //       console.log("Not in Node.js environment; `fs` module is not available.");
+  //       return null;
+  //     }
 
-      if (!fs.existsSync(".credentials.json")) {
-        throw new DimoError("Credentials file does not exist");
-      }
+  //     if (!fs.existsSync(".credentials.json")) {
+  //       throw new DimoError("Credentials file does not exist");
+  //     }
 
-      const data = fs.readFileSync(".credentials.json", "utf8");
-      const credentials = JSON.parse(data);
+  //     const data = fs.readFileSync(".credentials.json", "utf8");
+  //     const credentials = JSON.parse(data);
 
-      const authHeader = await this.auth.getToken({
-        client_id: credentials.client_id,
-        domain: credentials.redirect_uri,
-        private_key: credentials.private_key,
-      });
+  //     const authHeader = await this.auth.getToken({
+  //       client_id: credentials.client_id,
+  //       domain: credentials.redirect_uri,
+  //       private_key: credentials.private_key,
+  //     });
 
-      return authHeader;
-    } catch (error: any) {
-      // Handle file not existing and other errors
-      console.error("Failed to authenticate:", error.message);
-      // Decide whether to throw the error or handle it differently
-      throw new DimoError("Authentication failed");
-    }
-  }
+  //     return authHeader;
+  //   } catch (error: any) {
+  //     // Handle file not existing and other errors
+  //     console.error("Failed to authenticate:", error.message);
+  //     // Decide whether to throw the error or handle it differently
+  //     throw new DimoError("Authentication failed");
+  //   }
+  // }
 
   async stream(streamId: string, clientId: string, privateKey: string, log?: string) {
     try {
